d993e9fbff71414d50e28168a1adde2ebe532c91
Justin Dang <dangjustintime@gmail.com>
2017-08-16 13:14:15 -0500

    added field fill requirements to order

--- app/src/main/java/com/example/pizzamaker/order/OrderActivity.java
+++ app/src/main/java/com/example/pizzamaker/order/OrderActivity.java
@@ -7,16 +7,22 @@ import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.GridLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.view.Gravity;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.RadioGroup;
+import android.widget.Toast;
 
 import com.example.pizzamaker.R;
 import com.example.pizzamaker.model.Topping;
 import com.example.pizzamaker.thankYou.ThankYouActivity;
 import com.example.pizzamaker.toppings.ToppingsActivity;
+import com.squareup.picasso.Picasso;
 
 import java.util.ArrayList;
 
@@ -28,11 +34,16 @@ public class OrderActivity extends AppCompatActivity {
     private static final String EXTRA_SIZE = "size";
     private static final String EXTRA_TOPPINGS = "toppings";
     private static final int REQUEST_TOPPINGS = 123;
+
+    private ImageView pizzaImageView;
     private EditText nameEditText;
     private EditText emailEditText;
     private EditText phoneNumberEditText;
     private RadioGroup sizeRadioGroup;
     private RecyclerView toppingsRecyclerView;
+    boolean nameChanged = false;
+    boolean emailChanged = false;
+    boolean phoneNumberChanged = false;
 
     private Button toppingAddButton;
     private Button orderButton;
@@ -45,6 +56,8 @@ public class OrderActivity extends AppCompatActivity {
     private String size;
 
 
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -54,6 +67,7 @@ public class OrderActivity extends AppCompatActivity {
         actionBar.setTitle("Order");
         actionBar.setDisplayHomeAsUpEnabled(true);
 
+        pizzaImageView = (ImageView) findViewById(R.id.image_view_pizza_order);
         nameEditText = (EditText) findViewById(R.id.edit_text_name);
         emailEditText = (EditText) findViewById(R.id.edit_text_email);
         phoneNumberEditText = (EditText) findViewById(R.id.edit_text_phone);
@@ -61,6 +75,15 @@ public class OrderActivity extends AppCompatActivity {
         toppingAddButton = (Button) findViewById(R.id.button_add_topping);
         orderButton = (Button) findViewById(R.id.button_order);
         size = "Small";
+
+
+        //image loader for pizzaImageView
+        Picasso.with(this)
+                .load(getResources().getString(R.string.pizza_pic_url))
+                .into(pizzaImageView);
+
+
+        //default values for fields
 
         sizeRadioGroup.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
             @Override
@@ -82,6 +105,39 @@ public class OrderActivity extends AppCompatActivity {
         toppingsRecyclerAdapter = new ToppingsGridRecyclerAdapter(this, toppings);
         toppingsRecyclerView.setAdapter(toppingsRecyclerAdapter);
 
+        //check if fields are filled
+        nameEditText.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {}
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {}
+            @Override
+            public void afterTextChanged(Editable s) {
+                nameChanged = true;
+            }
+        });
+
+        emailEditText.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {}
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {}
+            @Override
+            public void afterTextChanged(Editable s) {
+                emailChanged = true;
+            }
+        });
+
+        phoneNumberEditText.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {}
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {}
+            @Override
+            public void afterTextChanged(Editable s) {
+                phoneNumberChanged = true;
+            }
+        });
 
         toppingAddButton.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -99,17 +155,25 @@ public class OrderActivity extends AppCompatActivity {
                 phoneNumber = phoneNumberEditText.getText().toString();
 
 
-                //build an intent and .putExtra pizza
-                Intent intent = new Intent(OrderActivity.this, ThankYouActivity.class);
-                intent.putExtra(EXTRA_NAME, name);
-                intent.putExtra(EXTRA_EMAIL, email);
-                intent.putExtra(EXTRA_PHONE_NUMBER, phoneNumber);
-                intent.putExtra(EXTRA_SIZE, size);
-                intent.putExtra(EXTRA_TOPPINGS, toppings);
 
-                //call startActivity() w/intent
-                startActivity(intent);
-                finish();
+
+                if(nameChanged == false || emailChanged == false || phoneNumberChanged == false) {
+                    Toast toast = Toast.makeText(OrderActivity.this, "Please fill out all fields", Toast.LENGTH_SHORT);
+                    toast.setGravity(Gravity.BOTTOM|Gravity.CENTER_HORIZONTAL, 0,0);
+                    toast.show();
+                } else {
+                    //build an intent and .putExtra pizza
+                    Intent intent = new Intent(OrderActivity.this, ThankYouActivity.class);
+                    intent.putExtra(EXTRA_NAME, name);
+                    intent.putExtra(EXTRA_EMAIL, email);
+                    intent.putExtra(EXTRA_PHONE_NUMBER, phoneNumber);
+                    intent.putExtra(EXTRA_SIZE, size);
+                    intent.putExtra(EXTRA_TOPPINGS, toppings);
+
+                    //call startActivity() w/intent
+                    startActivity(intent);
+                    finish();
+                }
             }
         });
     }
